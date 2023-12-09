89ec28775d27e86aa0e9367d8938ed5b587e7667
mrdil <mrdil@BMW>
2023-12-09 22:31:29 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace module15_pr_zd3
+{
+    class MyClass
+    {
+        private int privateField;
+        public int PublicField;
+
+        private MyClass() { }
+
+        public MyClass(int value)
+        {
+            privateField = value;
+            PublicField = value;
+        }
+
+        private void PrivateMethod()
+        {
+            Console.WriteLine("Private method called");
+        }
+
+        public void PublicMethod()
+        {
+            Console.WriteLine("Public method called");
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Type myClassType = typeof(MyClass);
+            object myObject = Activator.CreateInstance(myClassType);
+
+            // Вызов метода
+            myClassType.GetMethod("PublicMethod").Invoke(myObject, null);
+        }
+    }
+
+}
